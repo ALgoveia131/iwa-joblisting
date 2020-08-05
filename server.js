9a415d1b16736d423d149caed8aebc59c9d078c3
ALgoveia131 <alysson.goveia131@gmail.com>
2020-08-05 12:22:56 +0000

    MongoDB connection working

--- server.js
+++ server.js
@@ -2,5 +2,22 @@ const cors = require("cors");
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
 
-console.log("Test");
+var app = express();
+var port = (process.env.PORT || 3000);
+
+mongoose.connect(process.env.MONGODB, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb Error: ', err);
+    process.exit();
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB is connected');
+});
+
+
+app.listen(port, function (err) {
+    console.log("Listening on Port: " + port)
+});
